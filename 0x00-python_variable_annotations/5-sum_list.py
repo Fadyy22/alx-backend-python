b81e493ed90739a9b49e468d7c72eba1e3c444d2
Fadyy22 <fadyalaa441@gmail.com>
2024-01-11 22:41:52 +0200

    sum all numbers in a list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+module containing sum_list function
+"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """return sum of all numbers in a list"""
+    return sum(input_list)
