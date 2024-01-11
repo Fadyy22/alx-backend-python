9cbcd50a2d5a44289953f5de94254d980092c187
Fadyy22 <fadyalaa441@gmail.com>
2024-01-11 23:32:08 +0200

    add types for function

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+module containing safely_get_value function
+"""
+
+
+from typing import Any, Mapping, Union
+
+
+def safely_get_value(dct: Mapping, key: Any, default=None) -> Union[Any, None]:
+    """gets the value of a key in dct if the key exists"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
