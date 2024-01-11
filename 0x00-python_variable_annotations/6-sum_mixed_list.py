ec0f2e70585d6d0edbffdde59b45dd242b0a9f18
Fadyy22 <fadyalaa441@gmail.com>
2024-01-11 22:47:18 +0200

    sum floats and ints in list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -6,6 +6,6 @@ module containing sum_mixed_list function
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """return sum of all numbers in a list"""
     return sum(mxd_lst)
